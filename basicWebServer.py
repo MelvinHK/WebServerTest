b2f6224b97b3b4d0e1379e9106cfc9e98a109ab7
Melvin <53848567+MelvinHK@users.noreply.github.com>
2021-04-15 19:52:55 +1000

    Update basicWebServer.py

--- basicWebServer.py
+++ basicWebServer.py
@@ -1,27 +1,25 @@
 from socket import *
 
 serverPort = 80
-serverSocket = socket(AF_INET, SOCK_STREAM)
-# Prepare a server socket
-serverSocket.bind(('', serverPort))
-serverSocket.listen(1)
+serverSocket = socket(AF_INET, SOCK_STREAM) # Make TCP server socket
+serverSocket.bind(('', serverPort)) # Bind address and port number to the socket
+serverSocket.listen(1) # Listen to TCP requests from client, parameter is max number of connections
+print('Ready to serve...')
 while True:
-    print('Ready to serve...')
-    connectionSocket, addr = serverSocket.accept()
+    connectionSocket, addr = serverSocket.accept() # Create new socket dedicated to the client, TCP connection is now established
     try:
-        message = connectionSocket.recv(1024)
-        print(message)
-        filename = message.split()[1]
-        f = open(filename[1:])
+        message = connectionSocket.recv(1024) # Get request header (packet data) from client
+        print(message) # Print the request header
+        filename = message.split()[1] # Extract /simpleWeb.html from req header
+        f = open(filename[1:]) # Open the file
         outputdata = f.read()
-        # Send
-        connectionSocket.send('HTTP/1.1 200 OK\r\n\r\n'.encode())
-        for i in range(0, len(outputdata)):
+        connectionSocket.send('HTTP/1.1 200 OK\r\n\r\n'.encode()) # Send success status response to client
+        for i in range(0, len(outputdata)): # Display webpage by sending the file character by character
             connectionSocket.send(outputdata[i].encode())
         connectionSocket.send("\r\n".encode())
         connectionSocket.close()
-    except IOError:
-        connectionSocket.send('404 NOT FOUND\r\n\r\n'.encode())
+    except IOError: # If file name not found:
+        connectionSocket.send('404 NOT FOUND\r\n\r\n'.encode()) # Send fail status
         connectionSocket.close()
 serverSocket.close()
 sys.exit()
